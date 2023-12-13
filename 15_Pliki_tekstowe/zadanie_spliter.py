29f5283be82a05d48843396549f50fb3ae7c2613
daniel <D4N13L.5T0L4R3K@gmail.com>
2023-12-13 15:55:46 +0100

    Zadanie splitter!

--- 15_Pliki_tekstowe/zadanie_spliter.py
+++ 15_Pliki_tekstowe/zadanie_spliter.py
@@ -11,3 +11,18 @@ with open('C:\\Users\\localadmin\\PycharmProjects\\Python-2023\\15_Pliki_tekstow
             dane = l.strip().split(',')
             slownik = dict(zip(first_line, dane))
             print(slownik)
+"""
+suma Budget dla Director
+
+import csv
+import sys
+data = []
+with open(sys.argv[1]) as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        data.append(row)
+print(
+    sum([float(x['Budget']) for x in data if x['Director'] == sys.argv[2]])
+
+)
+"""
